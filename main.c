33b9edf69ec2c0b555ba5b0c965f90dc18d94cae
Yushmanov Anton <antoxa327@gmail.com>
2020-06-24 22:47:09 +0700

    добавлены примеры основных стурктур для последующей работы

--- main.c
+++ main.c
@@ -1,4 +1,21 @@
+struct Object
+{
+	int xPos;
+	int yPos;
+	int color_number;
+}
+
+init_pair(1, COLOR_RED, COLOR_BLACK);
+
 int main
 {
+	int map0[5][5] =
+	{
+		{1,1,1,1,1},
+		{1,0,0,1,1},
+		{1,0,2,0,1},
+		{1,4,0,5,1},
+		{1,1,1,1,1};
+	}
 	return 0;
 }
