f3ea621f51fc4a4228b20967fc1b9ebc4bdafd17
Yushmanov Anton <antoxa327@gmail.com>
2020-06-24 23:15:41 +0700

    фикс шаблона карты

--- main.c
+++ main.c
@@ -3,7 +3,11 @@ struct Object
 	int xPos;
 	int yPos;
 	int color_number;
-}
+};
+
+struct Object *Boxs;
+struct Object *Endpoints;
+struct Object Player;
 
 init_pair(1, COLOR_RED, COLOR_BLACK);
 
@@ -14,7 +18,7 @@ int main
 		{1,1,1,1,1},
 		{1,0,0,1,1},
 		{1,0,2,0,1},
-		{1,4,0,5,1},
+		{1,3,0,4,1},
 		{1,1,1,1,1};
 	}
 	return 0;
