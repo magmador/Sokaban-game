6097b190bca06412234aec6151c5dab64e291d2a
Yushmanov Anton <antoxa327@gmail.com>
2020-06-26 15:57:36 +0700

    добавлены функции для Level

--- sources/main.c
+++ sources/main.c
@@ -1,9 +1,5 @@
 #include "main.h"
 
-Object *Boxs; //массив ящиков
-Object *Endpoints; //массив эндпоинтов
-Object Player; //перменная игрока
-
 int main()
 {
 	int **map = NULL;
@@ -19,5 +15,26 @@ int main()
 		LevelOutput(map);
 	}
 #endif
+	size_t boxCount = 0;
+	Object *Boxs; //массив ящиков
+	//Object *Endpoints; //массив эндпоинтов
+	//Object Player; //перменная игрока
+	for(int row = 0; row < MAP_ROW_COUNT; row++)
+	{
+		for(int col = 0; col < MAP_COL_COUNT; col++)
+		{
+			if(boxCount == 0 && map[row][col] == 2)
+			{
+				boxCount++;
+				Boxs = (Object *)malloc(boxCount * sizeof(Object));
+			}
+			/*if (map[row][col] == 2)
+			{
+				boxCount++;
+				Boxs = (Object *)realloc(,boxCount * sizeof(Object));
+			}*/
+		}
+	}
+	printf("%ld\n", boxCount);
 	return 0;
 }
