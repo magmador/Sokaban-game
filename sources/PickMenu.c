db9e3e7c7ad74a58d8e81f0690e1a82d107d502f
kuro <kuro@KuroDeb.168.0.11>
2020-06-28 03:13:36 +0700

    Небольшой рефакторинг кода и фикс функции выбора в меню.

--- sources/PickMenu.c
+++ sources/PickMenu.c
@@ -2,36 +2,43 @@
  
  void PickMenu(WINDOW *menuWnd)
  {
-     int ch = getchar();
-     switch(ch)
-     {
-        case 's':
+    int ch = 0;
+    bool selected = false;
+    do
+    {
+        ch = getchar();
+        switch(ch)
         {
-            delwin(menuWnd);
-            clear();
-            refresh();  
-	        break; 
+           case 's':
+            {
+               delwin(menuWnd);
+               clear();
+               refresh();
+               selected = true;  
+               break; 
+            }
+            case 'm':
+            {
+                DrawMenu(menuWnd);
+                getch();
+                break; 
+            }
+            case 'q':
+            {
+                delwin(menuWnd);
+                clear();
+                refresh(); 
+                endwin();
+                selected = true;
+                exit(1); 
+                break; 
+            }
+            default:
+            {
+                //DrawMenu(menuWnd);
+                //getch();
+                break;  
+            }      
         }
-    	case 'm':
-        {
-            DrawMenu(menuWnd);
-            getch();
-            break; 
-        }
-        case 'q':
-        {
-            delwin(menuWnd);
-            clear();
-            refresh(); 
-            endwin();
-            exit(1); 
-            break; 
-        }
-        default:
-        {
-            DrawMenu(menuWnd);
-            getch();
-            break;  
-        }      
-    }
+    }while(!selected);
 }
